57e66509b3ff54ed7d8f351f8cd70ab66f729ff8
Nikita Savenko <Nikita.lol.d9@gmail.com>
2023-03-25 18:44:14 +0200

    Added class and interface for serialization

---
+++ SkillCourse/DataBaseStructure/serialize/Serialize.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SkillCourse.DataBaseStructure.serialize
+{
+    public class Serialize
+    {
+        public bool SerializeObject<T>(ref T obj, string fileName)
+        {
+            try
+            {
+                var stringJson = JsonSerializer.Serialize(obj);
+                File.WriteAllText(fileName + ".json", stringJson);
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool DeserializeObject<T>(ref T obj, string fileName)
+        {
+            try
+            {
+                if (File.Exists(fileName + ".json"))
+                {
+                    var stringJson = File.ReadAllText(fileName + ".json");
+                    T? deserializeObject = JsonSerializer.Deserialize<T>(stringJson);
+                    if (deserializeObject == null)
+                    {
+                        return false;
+                    }
+                    obj = deserializeObject;
+                    return true;
+                }
+                return false;
+            }
+            catch { return false; }
+        }
+
+    }
+}
---
+++ SkillCourse/DataBaseStructure/serialize/interfaces/ISerializableJSON.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillCourse.DataBaseStructure.serialize.interfaces
+{
+    public interface ISerializableJSON
+    {
+        public bool SerializeObject(string inFileName);
+        public bool DeserializeObject(string fromFileName);
+    }
+}
