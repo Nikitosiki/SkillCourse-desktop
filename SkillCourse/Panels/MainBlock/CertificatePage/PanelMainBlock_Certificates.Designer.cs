6d16be41f2c8d5ccc1543159cd65e955101cac2c
Nikitosiki <nikita.lol.d9@gmail.com>
2023-04-22 11:46:49 +0300

    Intial animation and checks to scroll through certificates

--- SkillCourse/Panels/MainBlock/CertificatePage/PanelMainBlock_Certificates.Designer.cs
+++ SkillCourse/Panels/MainBlock/CertificatePage/PanelMainBlock_Certificates.Designer.cs
@@ -29,22 +29,63 @@
         private void InitializeComponent()
         {
             flowLayoutPanel1 = new FlowLayoutPanel();
+            panelBlockScroll = new Panel();
+            buttonScrollRight = new Button();
+            buttonScrollLeft = new Button();
             SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.AutoScroll = true;
-            flowLayoutPanel1.Location = new Point(45, 17);
+            flowLayoutPanel1.Location = new Point(73, 17);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(917, 638);
+            flowLayoutPanel1.Size = new Size(866, 638);
             flowLayoutPanel1.TabIndex = 0;
             flowLayoutPanel1.WrapContents = false;
+            // 
+            // panelBlockScroll
+            // 
+            panelBlockScroll.Location = new Point(27, 629);
+            panelBlockScroll.Name = "panelBlockScroll";
+            panelBlockScroll.Size = new Size(936, 44);
+            panelBlockScroll.TabIndex = 1;
+            // 
+            // buttonScrollRight
+            // 
+            buttonScrollRight.FlatAppearance.BorderSize = 0;
+            buttonScrollRight.FlatStyle = FlatStyle.Flat;
+            buttonScrollRight.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonScrollRight.ForeColor = SystemColors.Control;
+            buttonScrollRight.Location = new Point(963, 270);
+            buttonScrollRight.Name = "buttonScrollRight";
+            buttonScrollRight.Size = new Size(46, 85);
+            buttonScrollRight.TabIndex = 2;
+            buttonScrollRight.Text = ">";
+            buttonScrollRight.UseVisualStyleBackColor = true;
+            buttonScrollRight.Click += buttonScrollRight_Click;
+            // 
+            // buttonScrollLeft
+            // 
+            buttonScrollLeft.FlatAppearance.BorderSize = 0;
+            buttonScrollLeft.FlatStyle = FlatStyle.Flat;
+            buttonScrollLeft.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonScrollLeft.ForeColor = SystemColors.Control;
+            buttonScrollLeft.Location = new Point(0, 270);
+            buttonScrollLeft.Name = "buttonScrollLeft";
+            buttonScrollLeft.Size = new Size(46, 85);
+            buttonScrollLeft.TabIndex = 3;
+            buttonScrollLeft.Text = "<";
+            buttonScrollLeft.UseVisualStyleBackColor = true;
+            buttonScrollLeft.Click += buttonScrollLeft_Click;
             // 
             // PanelMainBlock_Certificates
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(40, 49, 60);
+            Controls.Add(buttonScrollLeft);
+            Controls.Add(buttonScrollRight);
+            Controls.Add(panelBlockScroll);
             Controls.Add(flowLayoutPanel1);
             Name = "PanelMainBlock_Certificates";
             Size = new Size(1012, 673);
@@ -54,5 +95,8 @@
         #endregion
 
         private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panelBlockScroll;
+        private Button buttonScrollRight;
+        private Button buttonScrollLeft;
     }
 }
--- SkillCourse/Panels/MainBlock/CertificatePage/PanelMainBlock_Certificates.cs
+++ SkillCourse/Panels/MainBlock/CertificatePage/PanelMainBlock_Certificates.cs
@@ -22,5 +22,51 @@ namespace SkillCourse.Panels.MainBlock
             flowLayoutPanel1.Controls.Add(new Component_newCertificate());
             flowLayoutPanel1.Controls.Add(new Component_newCertificate());
         }
+
+        private void buttonScrollRight_Click(object sender, EventArgs e)
+        {
+            int scrollAmount = 865; // Заданное количество пикселей для прокрутки
+
+            int endPositionPanel = flowLayoutPanel1.HorizontalScroll.Value + scrollAmount;
+            if (endPositionPanel > flowLayoutPanel1.HorizontalScroll.Minimum &&
+                endPositionPanel < flowLayoutPanel1.HorizontalScroll.Maximum)
+                ScrollPanelAnimation(scrollAmount);
+        }
+
+        private void buttonScrollLeft_Click(object sender, EventArgs e)
+        {
+            int scrollAmount = -865; // Заданное количество пикселей для прокрутки
+
+            int endPositionPanel = flowLayoutPanel1.HorizontalScroll.Value + scrollAmount;
+            if (endPositionPanel > flowLayoutPanel1.HorizontalScroll.Minimum &&
+                endPositionPanel < flowLayoutPanel1.HorizontalScroll.Maximum)
+                ScrollPanelAnimation(scrollAmount);
+        }
+
+        private void ScrollPanelAnimation(int scrollAmount)
+        {
+            int currentScrollPosition = flowLayoutPanel1.HorizontalScroll.Value;
+            int targetScrollPosition = currentScrollPosition + scrollAmount;
+
+            // Анимация прокрутки
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 5; // Интервал обновления анимации (в миллисекундах)
+            timer.Tick += (s, args) =>
+            {
+                if (flowLayoutPanel1.HorizontalScroll.Value < targetScrollPosition)
+                {
+                    flowLayoutPanel1.HorizontalScroll.Value += 5; // Шаг анимации
+                }
+                else if (flowLayoutPanel1.HorizontalScroll.Value > targetScrollPosition)
+                {
+                    flowLayoutPanel1.HorizontalScroll.Value -= 5; // Шаг анимации
+                }
+                else
+                {
+                    timer.Stop(); // Останавливаем таймер после завершения анимации
+                }
+            };
+            timer.Start(); // Запускаем таймер для анимации
+        }
     }
 }
